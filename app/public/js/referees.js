928b3eedc6fb68f91c5c82410f8691c59c1dfd80
sananias <89755110+sananias@users.noreply.github.com>
2021-10-31 11:54:08 -0400

    Sachi's PHP and JS Updates

--- app/public/js/referees.js
+++ app/public/js/referees.js
@@ -1,29 +1,119 @@
 const Referee = {
-    data() {
-      return {
-        "referees": [],
-      }
-    },
-  
-
-    methods: {
-
-
-        fetchBooksData() {
-            fetch('/api/referees/')
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                this.referees = responseJson;
-            })
-            .catch( (err) => {
-                console.error(err);
-            })
-        },
-    },
-    created() {
-        this.fetchRefereesData();
+  data() {
+    return {
+      "referees":[],
+      refereeForm:{},
+      selectedReferee: null
     }
+  },
+methods: {
+// Currency Formatting ------------------------------------------------------------------------------------
+prettyDollar(n) {
+const d = new Intl.NumberFormat("en-US").format(n);
+  return "$ " + d;
+              },
+// API to SQL for Referees Table ------------------------------------------------------------------------------------
+fetchRefereesData() {
+fetch('/api/referees/index.php')
+.then( response => response.json() )
+.then( (responseJson) => {
+console.log(responseJson);
+this.referees = responseJson;
+})
+.catch( (err) => {
+console.error(err);
+  })
+},
+
+// Post New Referee from form ------------------------------------------------------------------------------------
+postReferee(evt) {
+if (this.selectedReferee === null) {
+    this.postNewReferee(evt);
+} else {
+    this.postEditReferee(evt);
+}
+},
+postNewReferee(evt) { 
+console.log("Create", this.refereeForm);
+
+fetch('api/referees/create.php', {
+    method:'POST',
+    body: JSON.stringify(this.refereeForm),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+             }
+  })
+
+  .then( response => response.json())
+  .then(json => {
+  console.log("Returned from post:", json);
+  this.referees = json;
+  // Clear whenever its submitted -------------
+  this.resetRefereeForm();
+               
+                 });
+            },    
+// Edit Referee ------------------------------------------------------------------------------------
+postEditReferee(evt) {
+
+console.log("Updating", this.refereeForm);
+
+fetch('api/referees/update.php', {
+    method:'POST',
+    body: JSON.stringify(this.refereeForm),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  })
+  .then( response => response.json() )
+  .then( json => {
+    console.log("Returned from post:", json);
   
+    this.referees = json;
+
+    // reset the form
+    this.resetRefereeForm();
+  });
+},
+//Delete Referee Record ------------------------------------------------------------------------------------
+postDeleteReferee(o) {
+if (!confirm("Are you sure you want to delete "+o.firstName+"'s record ?")) {
+  return;
+}
+console.log("Delete", o);
+
+fetch('api/referees/delete.php', {
+    method:'POST',
+    body: JSON.stringify(o),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  })
+  .then( response => response.json() )
+  .then( json => {
+    console.log("Returned from post:", json);
+    // TODO: test a result was returned!
+    this.referees = json;
+
+    // reset the form
+    this.resetRefereeForm();
+  });
+},
+selectRefereeToEdit(o) {
+  this.selectedReferee = o;
+  this.refereeForm = Object.assign({}, this.selectedReferee);
+},
+resetRefereeForm() {
+  this.selectedReferee = null;
+  this.refereeForm = {};
+}
+            
+ 
+// Closing Methods ------------------------------------------------------------------------------------
+},
+// Created ------------------------------------------------------------------------------------
+created() {
+      this.fetchRefereesData();
   }
-Vue.createApp(Referee).mount('#refereeTable');
+}
+Vue.createApp(Referee).mount('#refereeApp');
